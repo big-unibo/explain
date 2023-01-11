1d989b554caaed9352660dfbf9fec646c23c5b45
w4bo <you@example.com>
2023-01-11 13:06:09 +0000

    fix file permissions

--- intentional/build.sh
+++ intentional/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-sudo apt-get install libaio1 libaio-dev
+sudo apt-get -y install libaio1 libaio-dev
 docker-compose build --no-cache
--- intentional/init.sh
+++ intentional/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -exo
 
+############################################################
+# REMEMBER TO RUN THIS SCRIPT WITH bash AND NOT WITH sh
+############################################################
+
 echo "Replacing .env.example with .env"
 cp .env.example .env
 echo "Replacing src/main/resources/config.example.yml src/main/resources/config.yml"
@@ -11,23 +15,29 @@ cp ../web/js/config.example.js ../web/js/config.js
 P=$(pwd)
 echo $P
 
-if [ -d "src/main/python/venv" ] 
-then
-    echo "The virtual environment already exists" 
+cd src/main/python
+if [ -d venv ]; then
+    echo "venv already exists"
 else
-    echo "Creating the virtual environment" 
-    cd src/main/python
-    python -m venv venv
-    if [ -d "venv/bin" ]
-    then
-        source venv/bin/activate
-    else
-        source venv/Scripts/activate
+    if [[ "$(python -V 2>&1)" == "Python 3" ]]; then
+        echo "python found"
+        python -m venv venv
     fi
-    pip install -r requirements.txt
-    chmod -R 777 .
-    cd -
+    if [[ "$(python3 -V 2>&1)" =~ "Python 3" ]]; then
+        echo "python3 found"
+        python3 -m venv venv
+    fi
 fi
+
+if [ -d venv/bin ]; then
+    source venv/bin/activate
+else
+    source venv/Scripts/activate
+fi
+
+pip install -r requirements.txt
+chmod -R 777 venv
+cd -
 
 sed -i "s+\!HOME\!+${P}+g" src/main/resources/config.yml
 sed -i "s+\!HOME\!+${P}+g" .env
