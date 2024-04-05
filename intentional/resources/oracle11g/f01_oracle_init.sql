2eb1b22642a040513045fbbf5ad9aa9ab5a864ea
w4bo <you@example.com>
2024-04-05 14:44:16 +0000

    using my oracle db image

--- intentional/resources/oracle11g/f01_oracle_init.sql
+++ intentional/resources/oracle11g/f01_oracle_init.sql
@@ -8,7 +8,11 @@ create user ssb_flight identified by oracle;
 create user sales_v1 identified by oracle;
 create user purchase_v1 identified by oracle;
 create user salespurchase_v1 identified by oracle;
+create user cimice identified by oracle;
+create user meaasurement identified by oracle;
 
+grant all privileges to cimice;
+grant all privileges to meaasurement;
 grant all privileges to foodmart;
 grant all privileges to frenchelectricity;
 grant all privileges to frenchelectricityext;
@@ -19,6 +23,8 @@ grant all privileges to purchase_v1;
 grant all privileges to salespurchase_v1;
 
 grant read, write on directory oracle_dump to system;
+grant read, write on directory oracle_dump to cimice;
+grant read, write on directory oracle_dump to meaasurement;
 grant read, write on directory oracle_dump to foodmart;
 grant read, write on directory oracle_dump to frenchelectricity;
 grant read, write on directory oracle_dump to frenchelectricityext;
